14f2d185ebf440b6edd4c8e56fa13675a0c658c6
John Skoumbourdis <scoumbourdisj@gmail.com>
2019-07-14 17:18:51 +0300

    Adding Latest Codeigniter 4.0.0-beta.3

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\UserModel;
+
+class Home extends BaseController
+{
+
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
