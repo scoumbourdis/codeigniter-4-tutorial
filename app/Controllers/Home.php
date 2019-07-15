54bec46c00c92c0d04d57c8ccd6e095e052020ce
John Skoumbourdis <scoumbourdisj@gmail.com>
2019-07-15 17:47:00 +0300

    Adding our first static page from app/Views/home_page.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,7 +8,7 @@ class Home extends BaseController
 
 	public function index()
 	{
-		return view('welcome_message');
+		return view('home_page');
 	}
 
 	//--------------------------------------------------------------------
