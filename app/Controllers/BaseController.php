f1fa9269b528c30b971cff4522145d84236538a8
John Skoumbourdis <scoumbourdisj@gmail.com>
2019-07-16 15:46:48 +0300

    Add base_url instead of relative paths

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -25,7 +25,9 @@ class BaseController extends Controller
 	 *
 	 * @var array
 	 */
-	protected $helpers = [ ];
+	protected $helpers = [
+        'url'
+	];
 
 	/**
 	 * Constructor.
--- app/Views/home_page.php
+++ app/Views/home_page.php
@@ -11,17 +11,17 @@
   <title>Creative - Start Bootstrap Theme</title>
 
   <!-- Font Awesome Icons -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url(); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
   <!-- Plugin CSS -->
-  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
   <!-- Theme CSS - Includes Bootstrap -->
-  <link href="css/creative.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>css/creative.min.css" rel="stylesheet">
 
 </head>
 
@@ -247,15 +247,15 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo base_url(); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url(); ?>vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/creative.min.js"></script>
+  <script src="<?php echo base_url(); ?>js/creative.min.js"></script>
 
 </body>
 
